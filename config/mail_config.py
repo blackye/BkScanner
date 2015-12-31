5eeb7ee5e38c499cf3341cf4d7e76cb9c08853c0
BlackYe <hswjzhang@gmail.com>
2015-12-31 18:39:44 +0800

    email配置信息

--- config/mail_config.py
+++ config/mail_config.py
@@ -6,10 +6,10 @@
 #send email setting
 MAIL_HOST = "smtp.163.com"
 MAIL_PORT = "25"
-MAIL_USER = "secscan"
-MAIL_PASS = "sec@pps.tv"
+MAIL_USER = ""
+MAIL_PASS = ""
 MAIL_POSTFIX = "163.com"
 
 ###############################
 #receive email setting
-MAILTO_LIST = ["zhangqiang@qiyi.com"]
+MAILTO_LIST = ["yourmail@qq.com"]
